18ab25afb146f243ed7897267ebf42dd25e9974c
Aniket Kumbhar <akumbhar01@entrata,com>
2022-06-17 14:44:32 +0530

    Added single number string failing test case

--- app/src/test/java/com/assignment/harmaan/calculator/StringCalculatorTest.kt
+++ app/src/test/java/com/assignment/harmaan/calculator/StringCalculatorTest.kt
@@ -17,9 +17,15 @@ class StringCalculatorTest {
     fun `return 0 when input is empty string`() {
         assertEquals(0, calculator.add(""))
     }
+
     @Test
     fun `return 0 when input is null`() {
         assertEquals(0, calculator.add(null))
+    }
+
+    @Test
+    fun `return number when input is single number string`() {
+        assertEquals(8, calculator.add("8"))
     }
 
 }
