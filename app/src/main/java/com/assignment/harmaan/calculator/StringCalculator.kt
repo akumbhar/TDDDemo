5748a12eb12ab831d8c7de6e4bc9ea0e58240178
Aniket Kumbhar <akumbhar01@entrata,com>
2022-06-17 16:27:50 +0530

    Code refactor : For multiple numbers input

--- app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
+++ app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
@@ -6,7 +6,7 @@ class StringCalculator {
         if (numberString.isNullOrEmpty()) return 0
         val numberList = mutableListOf<Int>()
 
-        numberString.split(",")?.forEach {
+        numberString.split(",").forEach {
             numberList.add(
                 try {
                     it.trim().toInt()
