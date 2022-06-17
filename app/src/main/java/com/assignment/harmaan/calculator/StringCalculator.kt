ac708f549d9e3fe33c8e6c7876de1ac4a3e9f2d6
Aniket Kumbhar <akumbhar01@entrata,com>
2022-06-17 16:56:32 +0530

    Code refactor : new line delimiter

--- app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
+++ app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
@@ -5,18 +5,21 @@ class StringCalculator {
     fun add(numberString: String?): Int {
         if (numberString.isNullOrEmpty()) return 0
         val numberList = mutableListOf<Int>()
-
-        val updatedString = numberString.replace("\n", ",")
-        updatedString.split(",").forEach {
-            numberList.add(
-                try {
-                    it.trim().toInt()
-                } catch (e: NumberFormatException) {
-                    0
+        with(numberString) {
+            replace("\n", ",").apply {
+                split(",").forEach {
+                    numberList.add(
+                        try {
+                            it.trim().toInt()
+                        } catch (e: NumberFormatException) {
+                            0
+                        }
+                    )
                 }
-            )
+            }
+
+
         }
         return numberList.sum()
     }
-
 }
