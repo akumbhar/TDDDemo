06833679ef371d8f2841fb3e358dfcbbdd325653
Aniket Kumbhar <akumbhar01@entrata,com>
2022-06-17 15:41:36 +0530

    Implementation for single number return

--- app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
+++ app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
@@ -4,6 +4,7 @@ class StringCalculator {
 
     fun add(numberString: String?): Int {
         if (numberString.isNullOrEmpty()) return 0
+        return numberString.toInt()
         return -1
     }
 
