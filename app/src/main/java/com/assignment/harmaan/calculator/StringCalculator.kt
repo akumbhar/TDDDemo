97549f0caf57a74b4e3cc0fc27e55b9b01dffdd5
Aniket Kumbhar <akumbhar01@entrata,com>
2022-06-17 17:59:06 +0530

    Passing test : support different delimiter

--- app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
+++ app/src/main/java/com/assignment/harmaan/calculator/StringCalculator.kt
@@ -1,16 +1,26 @@
 package com.assignment.harmaan.calculator
 
-const val NEW_LINE_DELIMETER = "\n"
-const val COMMA = ","
+const val NEW_LINE_CHAR = "\n"
+const val DELIMITER_PREFIX = "//"
 
 class StringCalculator {
 
+    private var delimiter = ","
     fun add(numberString: String?): Int {
         if (numberString.isNullOrEmpty()) return 0
+
+        if (numberString.contains(DELIMITER_PREFIX)) {
+            val startIndex = numberString.indexOf(DELIMITER_PREFIX) + DELIMITER_PREFIX.length
+            val endIndex = numberString.indexOf(NEW_LINE_CHAR)
+            delimiter = numberString.substring(startIndex, endIndex)
+        } else {
+            delimiter = ","
+
+        }
         val numberList = mutableListOf<Int>()
         with(numberString) {
-            replace(NEW_LINE_DELIMETER, COMMA).run {
-                split(COMMA).forEach {
+            replace(NEW_LINE_CHAR, delimiter).run {
+                split(delimiter).forEach {
                     numberList.add(
                         try {
                             it.trim().toInt()
@@ -20,9 +30,8 @@ class StringCalculator {
                     )
                 }
             }
-
-
         }
         return numberList.sum()
     }
+
 }
